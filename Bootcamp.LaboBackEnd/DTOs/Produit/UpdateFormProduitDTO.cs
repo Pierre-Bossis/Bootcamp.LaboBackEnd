ce59ec33c86f99e959c5d1fc367ab6f0138f20f9
p.bossis <pbossis@hotmail.com>
2025-03-10 15:34:02 +0100

    amelioration validations dto

--- Bootcamp.LaboBackEnd/DTOs/Produit/UpdateFormProduitDTO.cs
+++ Bootcamp.LaboBackEnd/DTOs/Produit/UpdateFormProduitDTO.cs
@@ -1,13 +1,19 @@
-﻿using Bootcamp.LaboBackEnd.DTOs.Categorie;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Bootcamp.LaboBackEnd.DTOs.Produit;
 
 public class UpdateFormProduitDTO
 {
     public int Id { get; set; }
+    [Required]
+    [RegularExpression(@"^\S.*", ErrorMessage = "Le nom ne doit pas commencer par un ou plusieurs espaces.")]
     public string Nom { get; set; }
+    [Required, Range(0,100000)]
     public decimal Prix { get; set; }
+    [Required, Range(0,1000)]
     public int Quantite { get; set; }
+    [Required, RegularExpression(@"^\S.*", ErrorMessage = "La description ne doit pas commencer par un ou plusieurs espaces.")]
     public string Description { get; set; }
+    [Required]
     public int CategorieId { get; set; }
 }
--- Bootcamp.LaboBackEnd/DTOs/Utilisateur/LoginFormUtilisateurDTO.cs
+++ Bootcamp.LaboBackEnd/DTOs/Utilisateur/LoginFormUtilisateurDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace Bootcamp.LaboBackEnd.DTOs.Utilisateur;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bootcamp.LaboBackEnd.DTOs.Utilisateur;
 
 public class LoginFormUtilisateurDTO
 {
+    [Required, EmailAddress, MinLength(2)]
     public string Email { get; set; }
+    [Required, DataType(DataType.Password)]
     public string Password { get; set; }
 }
