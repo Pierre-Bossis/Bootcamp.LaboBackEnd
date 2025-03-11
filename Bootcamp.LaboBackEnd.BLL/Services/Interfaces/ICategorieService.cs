eb2c904eb0cd299fded2a2cb3f7722566ee7ad4e
p.bossis <pbossis@hotmail.com>
2025-03-11 14:02:02 +0100

    commentaires bll interfaces

--- Bootcamp.LaboBackEnd.BLL/Services/Interfaces/ICategorieService.cs
+++ Bootcamp.LaboBackEnd.BLL/Services/Interfaces/ICategorieService.cs
@@ -4,8 +4,27 @@ namespace Bootcamp.LaboBackEnd.BLL.Services.Interfaces;
 
 public  interface ICategorieService
 {
+    /// <summary>
+    /// Récupérer toutes les catégories.
+    /// </summary>
+    /// <returns>Une IEnumerable de <see cref="Categorie"/></returns>
     IEnumerable<Categorie> GetAllCategories();
+    /// <summary>
+    /// Récupérer une catégorie par son Id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Une instance de <see cref="Categorie"/></returns>
     Categorie GetCategorieById(int id);
+    /// <summary>
+    /// Récupérer une catégorie par son Nom.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns>Une instance de <see cref="Categorie"/></returns>
     Categorie GetCategorieByName(string name);
+    /// <summary>
+    /// Créer une catégorie.
+    /// </summary>
+    /// <param name="categorie"></param>
+    /// <returns>L'instance de la <see cref="Categorie"/> construite.</returns>
     Categorie AddCategorie(string categorie);
 }
--- Bootcamp.LaboBackEnd.BLL/Services/Interfaces/IUtilisateurService.cs
+++ Bootcamp.LaboBackEnd.BLL/Services/Interfaces/IUtilisateurService.cs
@@ -4,8 +4,29 @@ namespace Bootcamp.LaboBackEnd.BLL.Services.Interfaces;
 
 public interface IUtilisateurService
 {
+    /// <summary>
+    /// Créer un compte.
+    /// </summary>
+    /// <param name="utilisateur"></param>
     void Register(Utilisateur utilisateur);
+    /// <summary>
+    /// Se connecter à un compte.
+    /// </summary>
+    /// <param name="email"></param>
+    /// <param name="password"></param>
+    /// <returns></returns>
     Utilisateur Login(string email, string password);
+    /// <summary>
+    /// Obtenir l'historique de commande pour un utilisateur.
+    /// </summary>
+    /// <param name="utilisateurId"></param>
+    /// <returns></returns>
     IEnumerable<Commande> historiqueCommandesByUtilisteurId(Guid utilisateurId);
+    /// <summary>
+    /// Modifier un utilisateur.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="utilisateur"></param>
+    /// <returns></returns>
     Utilisateur Update(Guid id, Utilisateur utilisateur);
 }
